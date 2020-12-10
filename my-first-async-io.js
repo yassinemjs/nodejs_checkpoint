784d8fdc2d9a88f462dd06858d87eab3c308760a
Yassine Mohamed <yassine.mohamed.mjs@gmail.com>
2020-12-10 17:00:45 +0100

    second commit

--- my-first-async-io.js
+++ my-first-async-io.js
@@ -1,5 +1,5 @@
 var fs = require('fs')
 var file = process.argv[2]
 fs.readFile(file ,'utf8' ,(err,data) => { 
-var n =data.toString.split('\n').length -1 
+var n =data.toString().split('\n').length -1 
 console.log(n) })
